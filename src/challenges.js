db348e4ade60a8dda49f944da754eaa995cc5fb4
Jeazi Ricardo <jeazi_ricardo@hotmail.com>
2022-02-08 17:23:16 -0300

    Crie uma função que use concatenação de strings

--- src/challenges.js
+++ src/challenges.js
@@ -19,8 +19,9 @@ function splitSentence(string) {
 }
 
 // Desafio 4
-function concatName() {
-  // seu código aqui
+function concatName(string) {
+  let array = string[string.length -1]  + ", " + string[0];
+  return array;
 }
 
 // Desafio 5
