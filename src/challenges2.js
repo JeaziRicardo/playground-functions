3309e505c0b393b278731629029a8c3a8f7e1c1e
Jeazi Ricardo <jeazi_ricardo@hotmail.com>
2022-02-10 09:44:55 -0300

    BONUS-Crie uma função de Número de Telefone

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,10 +1,27 @@
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(array) {
+  let telNumber = array.join("");
+  if (array.length !== 11) {
+    return "Array com tamanho incorreto.";
+  }
+  for(index of array) {
+    let counter = 0;
+    for (index2 of array) {
+      if (index === index2) {  
+        counter += 1;
+      }
+    }
+    if (counter >= 3 || index < 0 || index > 9) {
+      return "não é possível gerar um número de telefone com esses valores";
+    }
+  }
+  return  "(" + telNumber.substring(0, 2) + ") " + telNumber.substring(2, 7) + "-" + telNumber.substring(7, 11);
 }
+// Foi usado o ".join()" para transformar o array em uma string e utilizar as propriedades de ".substring()". Informações retiradas da documentação.
+// Links: .Join() - [https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Array/join#example_joining_an_array_three_different_ways], .substring() - [https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/String/substring].
 
 // Desafio 12
-function triangleCheck() {
+function triangleCheck() {''
   // seu código aqui
 }
 
